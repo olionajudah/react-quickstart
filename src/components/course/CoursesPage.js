e0ece61a22d2563d8f21a1ca7f09e7c6ff9a8051
michael.markus <michael.markus@disney.com>
2017-09-20 09:11:07 -0700

    setting up for redux flow

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,42 @@
 import React, { Component, PropTypes } from 'react';
 
 class CoursesPage extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+
+        this.state = {
+            course: {title: ""}
+        };
+    }
+
+    onTitleChange(event) {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course: course});
+    }
+
+    onClickSave(event) {
+        alert(`Saving ${this.state.course.title}`);
+    }
+
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                <h2>Add Course</h2>
+                <input 
+                    type="text"
+                    onChange={this.onTitleChange}
+                    value={this.state.course.title} 
+                />
+                <input
+                    type="submit"
+                    onClick={this.onClickSave} 
+                    value="Save"
+                />
             </div>
         );
     }
